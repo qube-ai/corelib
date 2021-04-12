a50f555abeb83155ddefa903f5880c5264fc1d2c
Apoorva Singh <apoorvasingh157@gmail.com>
2021-04-12 21:10:48 +0530

    Version updated to 1.1.6

--- src/corelib.h
+++ src/corelib.h
@@ -1,35 +1,35 @@
 #include <Arduino.h>
 
 #if defined(CORELIB_STORAGE)
-#include "common/Storage.h"
+    #include "common/Storage.h"
 #endif
 
 #if defined(CORELIB_WIFI_MANAGER)
-#include "common/WiFiManager.h"
+    #include "common/WiFiManager.h"
 #endif
 
 #if defined(CORELIB_APP_COMM)
-#include "common/appComm.h"
+    #include "common/appComm.h"
 #endif
 
 #if defined(CORELIB_STATUS_LED)
-#include "common/statusLed.h"
+    #include "common/statusLed.h"
 #endif
 
 #if defined(ESP32) && defined(CORELIB_FOTA)
-#include "esp32/FOTA_esp32.h"
+    #include "esp32/FOTA_esp32.h"
 #endif
 
 #if defined(ESP32) && defined(CORELIB_IOTCORE)
-#include "esp32/IoTCore_esp32.h"
+    #include "esp32/IoTCore_esp32.h"
 #endif
 
 #if defined(ESP8266) && defined(CORELIB_FOTA)
-#include "esp8266/FOTA_esp8266.h"
+    #include "esp8266/FOTA_esp8266.h"
 #endif
 
 #if defined(ESP8266) && defined(CORELIB_IOTCORE)
-#include "esp8266/IoTCore_esp8266.h"
+    #include "esp8266/IoTCore_esp8266.h"
 #endif
 
 namespace corelib {
